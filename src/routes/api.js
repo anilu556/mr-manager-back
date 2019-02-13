dbc4d1aa1c893b0293ec1fd02b9d819107ca7e83
anilu556 <anilu556@hotmail.com>
2019-02-13 11:18:12 -0600

    initializing back end

---
+++ src/routes/api.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const app = Router();
+
+// const Companies = require('../controllers/companies/companies');
+//
+// //company routes
+// app.get('/companies', Companies.index);
+// app.post('/companies', Companies.create);
+// app.get('/companies/:id', Companies.findBy);
+// app.delete('/companies/:id', Companies.deleteBy);
+// app.put('/companies/:id', Companies.update);
+
+module.exports = app;
